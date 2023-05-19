e2759b7623eba940779aa6fb2c223d3171a5aa0f
auch3rwahom3 <aucherwahome@gmail.com>
2023-05-19 16:01:37 -0700

    cant get the signup to work

--- back/server.js
+++ back/server.js
@@ -4,10 +4,45 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors);
+app.use(express.json());
 
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password:"",
-    database: ""
+    database: "signup"
+})
+
+app.post('/signup',(req, res) => {
+    const sql ="INSERT INTO login (`name`,`email`,`password`) VALUES (?)";
+    const values =[
+        req.body.name,
+        req.body.email,
+        req.body.password
+    ]
+    db.query(sql, [values],(err, data) => {
+        if(err){
+            return res.json("Error");
+        }
+        return res.json(data);
+    })
+})
+
+app.post('/login',(req, res) => {
+    const sql ="SELECT * FROM login WHERE `email` = ? AND `password` = ? ";
+    
+    db.query(sql, [req.body.email, req.body.password],(err, data) => {
+        if(err){
+            return res.json("Error");
+        }
+       if(data.lengtj >0){
+        return res.json("Success");
+       } else{
+        return res.json("Failed");
+       }
+    })
+})
+
+app.listen(3000, ()=> {
+    console.log("listening");
 })

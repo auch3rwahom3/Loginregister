2a648654803c536300fd53a78c8af2b9b3c16dcc
auch3rwahom3 <aucherwahome@gmail.com>
2023-05-18 23:46:06 -0700

    finsihed up with the frontend of the login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Login from './Login';
-import {BrowserRouter, Routes, route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Signup from './Signup';
+
 function App() {
   return (
     <BrowserRouter>
